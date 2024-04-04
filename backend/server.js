33a0e6d93309ed6f2b6ae1bf9a2b3119742c56b0
Cole Maring <65455664+colemaring@users.noreply.github.com>
2024-04-03 21:38:45 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -22,9 +22,22 @@ const signupRoute = require("./routes/signup.js");
 const loginRoute = require("./routes/login.js");
 const createUniRoute = require("./routes/createuni.js");
 const fetchUniRoute = require("./routes/fetchunis.js");
+const createEventRoute = require("./routes/createevent.js");
+const fetchEventsRoute = require("./routes/fetchevents.js");
+const rso_create_request = require("./routes/rso_create_request.js");
+const fetchRsoRequests = require("./routes/fetchrsorequests.js");
+const denyRequest = require("./routes/denyrequest.js");
+// const acceptRequest = require("./routes/acceptrequest.js");
+
 app.use("/signup", signupRoute);
 app.use("/login", loginRoute);
 app.use("/createuni", createUniRoute);
 app.use("/fetchunis", fetchUniRoute);
+app.use("/createevent", createEventRoute);
+app.use("/fetchevents", fetchEventsRoute);
+app.use("/rso_create_request", rso_create_request);
+app.use("/fetchrsorequests", fetchRsoRequests);
+app.use("/denyrequest", denyRequest);
+// app.use("/acceptrequest", acceptRequest);
 
 module.exports = db;
